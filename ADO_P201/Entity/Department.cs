40aedada807eee1bd536289a433435cd02385b17
Semeniuk Oleg <75548640+dsgnrr@users.noreply.github.com>
2023-02-08 10:26:21 +0200

    5 lesson practice

---
+++ ADO_P201/Entity/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_P201.Entity
+{
+    // ОБОВ'ЯЗКОВО public class
+    // ORM-сутність — клас, що відображає таблицю Departments(рядок таблиці)
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public String name { get; set; }
+
+    }
+}
---
+++ ADO_P201/OrmWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADO_P201
+{
+
+    public partial class OrmWindow : Window
+    {
+        public ObservableCollection<Entity.Department> Departments { get; set; }
+
+        public OrmWindow()
+        {
+            InitializeComponent();
+            Departments = new();
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
