8fca4c94ba845b44dc3126b3b43508a33467d471
dsgnrr <75548640+dsgnrr@users.noreply.github.com>
2023-02-23 10:10:14 +0200

    lesson 14

---
+++ ADO_P201/DAL/DepartmentApi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ADO_P201.DAL
+{
+    internal class DepartmentApi
+    {
+        private readonly SqlConnection _connection;
+        public DepartmentApi(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public List<Entity.Department> GetAll()
+        {
+            var list = new List<Entity.Department>();
+            try
+            {
+                using SqlCommand cmd = new()
+                {
+                    Connection = _connection,
+                    CommandText = @"SELECT S.*
+                                    FROM Sales S 
+                                    WHERE DeleteDt IS NULL"
+                };
+                
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                    list.Add(new(reader));
+            }
+            catch(Exception ex)
+            {
+                String msg =
+                    DateTime.Now + ": " +
+                    this.GetType().Name +
+                    System.Reflection.MethodBase.GetCurrentMethod()?.Name +
+                    " " + ex.Message;
+                
+                // TODO: LOG
+                MessageBox.Show(msg);
+            }
+            //reader.Close();
+            //cmd.Dispose();
+            return list;
+        }
+    }
+}
